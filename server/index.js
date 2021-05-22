e135d7788a25725d6e6119cc104db851e777681a
Lexy Kramer <lexy.kramer@gmail.com>
2021-05-22 19:14:39 -0400

    Initialized server using express

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(express.urlencoded());
+
+const PORT = 3001;
+app.use(
+  cors({
+    origin: `http://localhost:3000`,
+  })
+);
+
+app.get("/register", (req, res) => {
+  res.send("here you go");
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Now listening on port http://localhost:${PORT}`);
+});
